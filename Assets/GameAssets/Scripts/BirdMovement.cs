35640a58babffa29be4ac9f1103f49955839b54a
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-11-24 16:50:43 +0300

    finger jump

--- Assets/GameAssets/Scripts/BirdMovement.cs
+++ Assets/GameAssets/Scripts/BirdMovement.cs
@@ -1,20 +1,34 @@
-
 using UnityEngine;
-
+using Lean.Touch;
 public class BirdMovement : MonoBehaviour
 {
+    [Header("Bird Movement Settings")]
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float jumpRotationSpeed = 5f;
     [SerializeField] private float fallRotationSpeed = 5f;
+    [SerializeField] private float jumpRotation = 45f;
+    [SerializeField] private float fallRotation = -45f;
+
+    [Header("References")]
     [SerializeField] private Rigidbody2D rb;
-
+    [SerializeField] private SpriteRenderer sprite;  
     private Quaternion targetRotation;
-
+    void OnEnable()
+	{
+		LeanTouch.OnFingerTap += HandleFingerTap;
+	}
+	void OnDisable()
+	{
+		LeanTouch.OnFingerTap -= HandleFingerTap;
+	}
+    void HandleFingerTap(Lean.Touch.LeanFinger finger)
+	{
+        rb.velocity = Vector2.up * jumpForce;
+	}
     private void Update()
     {
         var rotationSpeed = rb.velocity.y >= 0 ? jumpRotationSpeed : fallRotationSpeed;
-        targetRotation = rb.velocity.y >= 0 ? Quaternion.Euler(0, 0, 45) : Quaternion.Euler(0, 0, -45);
-        if (Input.GetKeyDown(KeyCode.Space)) {rb.velocity = Vector2.up * jumpForce;};
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        targetRotation = rb.velocity.y >= 0 ? Quaternion.Euler(0, 0, jumpRotation) : Quaternion.Euler(0, 0, fallRotation);
+        sprite.transform.rotation = Quaternion.Lerp(sprite.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
     }
 }
