46421fbcac56608f49b2a8a315008f831660dbca
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-11-29 16:20:50 +0300

    get leaderboard max value data from playfab

--- Assets/GameAssets/Scripts/Manager/ScoreManager.cs
+++ Assets/GameAssets/Scripts/Manager/ScoreManager.cs
@@ -1,73 +1,108 @@
-using UnityEngine;
-using System.Collections.Generic;
-using PlayFab.ClientModels;
-using PlayFab;
+    using UnityEngine;
+    using System.Collections.Generic;
+    using PlayFab.ClientModels;
+    using PlayFab;
 
-public class ScoreManager : MonoBehaviour
-{
-    private int score = 0;
-    private int highScore = 0;
+    public class ScoreManager : MonoBehaviour
+    {
+        private int score = 0;
+        private int highScore = 0;
 
-    private void OnEnable()
-    {
-        EventManager.AddHandler(GameEvent.OnScore, AddScore);
-    }
-
-    private void OnDisable()
-    {
-        EventManager.RemoveHandler(GameEvent.OnScore, AddScore);
-    }
-    private void Start()
-    {
-        highScore = PlayerPrefs.GetInt("HighScore", 0);
-    }
-
-    private void AddScore()
-    {
-        score++;
-        if (score > highScore)
+        private void OnEnable()
         {
-            highScore = score;
-            PlayerPrefs.SetInt("HighScore", highScore);
-            try
-            {
-                var request = new UpdatePlayerStatisticsRequest
-                {
-                    Statistics = new List<StatisticUpdate>
-                {
-                    new StatisticUpdate
-                    {
-                        StatisticName = "FlappyBird",
-                        Value = highScore
-                    }
-                }
-                };
-                PlayFabClientAPI.UpdatePlayerStatistics(request, OnUpdatePlayerStatisticsSuccess, OnUpdatePlayerStatisticsError);
-            }
-            catch
-            {
-                Debug.Log("Error submitting score");
-            }
-
+            EventManager.AddHandler(GameEvent.OnScore, AddScore);
         }
-        EventManager.TriggerEvent(GameEvent.OnUpdateUI);
-    }
 
-    private void OnUpdatePlayerStatisticsSuccess(UpdatePlayerStatisticsResult result)
-    {
-        Debug.Log("Successfully submitted score");
+        private void OnDisable()
+        {
+            EventManager.RemoveHandler(GameEvent.OnScore, AddScore);
+        }
+
+        private void Start()
+        {
+            GetHighScoreFromLeaderboard();
+        }
+
+        private void GetHighScoreFromLeaderboard()
+        {
+            var request = new GetLeaderboardRequest
+            {
+                StatisticName = "FlappyBird",
+                StartPosition = 0,
+                MaxResultsCount = 1
+            };
+
+            PlayFabClientAPI.GetLeaderboard(request, OnGetLeaderboardSuccess, OnGetLeaderboardError);
+        }
+
+        private void OnGetLeaderboardSuccess(GetLeaderboardResult result)
+        {
+            if (result.Leaderboard.Count > 0)
+            {
+                highScore = result.Leaderboard[0].StatValue;
+            }
+            else
+            {
+                highScore = 0;
+            }
+
+            PlayerPrefs.SetInt("HighScore", highScore);
+        }
+
+        private void OnGetLeaderboardError(PlayFabError error)
+        {
+            Debug.Log("Error getting leaderboard: " + error.GenerateErrorReport());
+        }
+
+        private void AddScore()
+        {
+            score++;
+            if (score > highScore)
+            {
+                highScore = score;
+                PlayerPrefs.SetInt("HighScore", highScore);
+                try
+                {
+                    var request = new UpdatePlayerStatisticsRequest
+                    {
+                        Statistics = new List<StatisticUpdate>
+                        {
+                            new StatisticUpdate
+                            {
+                                StatisticName = "FlappyBird",
+                                Value = highScore
+                            }
+                        }
+                    };
+                    PlayFabClientAPI.UpdatePlayerStatistics(request, OnUpdatePlayerStatisticsSuccess, OnUpdatePlayerStatisticsError);
+                }
+                catch
+                {
+                    Debug.Log("Error submitting score");
+                }
+
+            }
+            EventManager.TriggerEvent(GameEvent.OnUpdateUI);
+        }
+
+        private void OnUpdatePlayerStatisticsSuccess(UpdatePlayerStatisticsResult result)
+        {
+            Debug.Log("Successfully submitted score");
+        }
+
+        private void OnUpdatePlayerStatisticsError(PlayFabError error)
+        {
+            Debug.Log("Error submitting score");
+            Debug.Log(error.GenerateErrorReport());
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
+
+        public int GetHighScore()
+        {
+            return highScore;
+        }
     }
-    private void OnUpdatePlayerStatisticsError(PlayFabError error)
-    {
-        Debug.Log("Error submitting score");
-        Debug.Log(error.GenerateErrorReport());
-    }
-    public int GetScore()
-    {
-        return score;
-    }
-    public int GetHighScore()
-    {
-        return highScore;
-    }
-}
